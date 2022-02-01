9fb449126e83d57a5b587ff8597eac46b35a885e
arjun-baidya <arjun.chandra.baidya@gmail.com>
2022-02-01 16:36:02 +0600

    first commit

---
+++ functions/functions.py
@@ -0,0 +1,22 @@
+# function are used to perform some action
+# function is called when we call it
+# A Python function is a group of code. ... A function can be called from anywhere after the function is defined. Functions can return a value using a return statement. Functions are a common feature among all programming languages
+
+# to return a value from a function, use the return statement
+# to avoid repeating the same logic over and over again, we can use functions
+
+
+# parameters are variables that are used to pass values to a function
+# information that is passed to a function is called an argument
+# parameter are also called arguments
+
+import os
+calculation_to_units = 24
+name_of_units = "hours"
+
+def days_to_units(name_of_days):
+    # dynamic print
+    print("{} {}".format(name_of_days, name_of_units))
+    print("ok")
+
+days_to_units(35)
