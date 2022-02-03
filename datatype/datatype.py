eb0885dfbc2477cf8ea2e67c20ba248b654e4c14
arjun-baidya <arjun.chandra.baidya@gmail.com>
2022-02-03 17:45:06 +0600

    update

---
+++ datatype/datatype.py
@@ -0,0 +1,11 @@
+# python data type is a collection of data types
+# data type is a collection of values
+
+# Python consists of six data-types
+
+a = 4
+b= 5.8
+c = 5j
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ datatype/list.py
@@ -0,0 +1,15 @@
+# A list in Python is used to store the sequence of various types of data
+#Python lists are mutable type its mean we can modify its element after it created
+# A list can be defined as a collection of values or items of different types. The items in the list are separated with the comma (,) and enclosed with the square brackets [].
+
+# Characteristics of Lists
+# The lists are ordered.
+# The element of the list can access by index.
+# The lists are the mutable type.
+# The lists are mutable types.
+# A list can store the number of various elements.
+
+list1 = ['arjun','kumar','kumar','arjun',2,'ripon']
+list2 = [1,2,3,4,5,6,7,8,9,10]
+print(list1)
+print(list2)
